efaa963604e0b61915d247c80a57976e3e503ceb
Roberto Barros <roberto.f.barros@gmail.com>
2016-08-11 18:07:30 -0300

    Doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:show, :index, :new, :create]
+
+  resources :cocktails, only: [:show, :index, :new, :create] do
+  	resources :doses, only: [:new, :create, :destroy]
+  end
   root "cocktails#index"
 end
